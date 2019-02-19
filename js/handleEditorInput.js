fb3c778c58f1cb3741510f6899acacbb506db59a
erv <ervinmitra@gmail.com>
2019-02-19 12:08:03 -0800

    Resolves #1

--- js/handleEditorInput.js
+++ js/handleEditorInput.js
@@ -3,12 +3,18 @@
  */
 
 export default function handleEditorInput(event){
+    const textEl = 
+        event.currentTarget.parentNode.querySelector
+        ('.text-element');
     if(event.target.type === 'text' ||
         event.target.tagName === 'TEXTAREA'){
-            const textEl = 
-                event.target.parentNode.querySelector
-                ('.text-element');
             const text = event.target.value;
             textEl.innerText = text;
-        }
+    }
+    else{
+        // handle styles
+        const {value, name: styleName} = event.target;
+        const unit = event.target.dataset.unit || '';
+        textEl.style[styleName] = `${value}${unit}`;
+    }
 }
